a40100d1ed6ac04cb63f7f91256d98504cd9bd6d
Tarah <tarah.agbokhana@gmail.com>
2021-04-16 14:14:58 -0400

    TodosService

--- src/main/java/com/lambdaschool/todos/services/TodosService.java
+++ src/main/java/com/lambdaschool/todos/services/TodosService.java
@@ -1,6 +1,12 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.models.Todos;
+
 public interface TodosService
 {
     void markComplete(long todoid);
+
+    Todos update(Todos todo, Long id);
+
+    Todos findUserById(Long id);
 }
--- src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
+++ src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
@@ -1,13 +1,52 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.models.Todos;
+import com.lambdaschool.todos.repository.TodosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Transactional
 @Service(value = "TodoService")
 public class TodosServiceImpl implements TodosService{
+
+  @Autowired
+  private TodosRepository todosRepos;
+
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private UserAuditing userAuditing;
+
+  @Override
+  public Todos findUserById(Long id) throws EntityNotFoundException {
+    return todosRepos.findById(id)
+                     .orElseThrow(() -> new EntityNotFoundException("User id " + id + " not found!"));
+  }
+
   @Override
   public void markComplete(long todoid) {
 
+  }
+
+  @Override
+  public Todos update(
+      Todos todos,
+      Long id) {
+
+    Todos currentTodo = findUserById(id);
+
+    if (todos.getDescription() != null){
+      currentTodo.setDescription(todos.getDescription());
+    }
+
+    //ManyToOne
+    if (todos.getUser() != null) {
+      todos.setUser(userService.findUserById(todos.getUser().getUserid()));
+    }
+    return todosRepos.save(currentTodo);
   }
 }
